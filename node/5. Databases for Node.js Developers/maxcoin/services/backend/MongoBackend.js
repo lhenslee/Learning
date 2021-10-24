3a642fc24d2764dbacd4e67339d03e9f16bdca93
Lane Henslee <lhenslee@vols.utk.edu>
2021-10-23 22:54:19 -0700

    wrote mongo backend connect and disconnect

--- node/5. Databases for Node.js Developers/maxcoin/services/backend/MongoBackend.js
+++ node/5. Databases for Node.js Developers/maxcoin/services/backend/MongoBackend.js
@@ -1,23 +1,57 @@
 /* eslint-disable no-useless-constructor */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-empty-function */
-const CoinAPI = require('../CoinAPI');
+
+const { MongoClient } = require("mongodb");
+
+const CoinAPI = require("../CoinAPI");
 
 class MongoBackend {
-
   constructor() {
     this.coinAPI = new CoinAPI();
+    this.mongoUrl = "mongodb://localhost:37017/maxcoin";
+    this.client = null;
+    this.collection = null;
   }
 
-  async connect() {}
+  async connect() {
+    const mongoClient = new MongoClient(this.mongoUrl, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    this.client = await mongoClient.connect();
+    this.collection = this.client.db("maxcoin").collection("values");
+    return this.client;
+  }
 
-  async disconnect() {}
+  async disconnect() {
+    if (this.client) {
+      return this.client.close();
+    }
+    return false;
+  }
 
   async insert() {}
 
   async getMax() {}
 
-  async max() {}
+  async max() {
+    console.info("Connection to MongoDB");
+    console.time("mongodb-connect");
+    try {
+      await this.connect();
+    } catch (error) {
+      console.log("MongoDB connection failure.");
+    }
+    console.log("MongoDB connection succuss.");
+    console.timeEnd("mongodb-connect");
+
+    // Disconnect from the database and time it
+    console.info("Disconnection to MongoDB");
+    console.time("mongodb-disconnect");
+    await this.disconnect();
+    console.timeEnd("mongodb-disconnect");
+  }
 }
 
 module.exports = MongoBackend;
